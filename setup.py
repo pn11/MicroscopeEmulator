a51918ee18baf72f852edbaeb1c7a5495b8213fe
oka <pn11@users.noreply.github.com>
2021-02-15 18:01:46 +0900

    disable python version limitation

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
 import sys
 
-if sys.version_info[:2] < (3, 7):
-    raise RuntimeError("Python version >= 3.7 required.")
-
+#if sys.version_info[:2] < (3, 7):
+#    raise RuntimeError("Python version >= 3.7 required.")
 
 with open('README.md') as f:
     readme = f.read()
